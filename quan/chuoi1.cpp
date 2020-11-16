ed0680c46d57e143dbf6a813dcab0aac0ce8df6b
thanhdrink247 <thanhkhoeo2@gmail.com>
2020-11-16 21:35:38 +0700

    quan up code chuoi1

---
+++ quan/chuoi1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+main ()
+{
+   string sum1 = "kho qua anh Thanh oi";
+   int len;
+   
+   cout << "Khi gap bai kho, em noi : " << sum1;
+   
+}
