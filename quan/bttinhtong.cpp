7c1ed6e1d13370983d87b63371366704044ac014
thanhdrink247 <thanhkhoeo2@gmail.com>
2020-12-16 17:46:54 +0700

    quan up code tinh tong

---
+++ quan/bttinhtong.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main(){
+int a , b , c , sum;
+printf("nhap a:");
+scanf("%d",&a);
+printf("nhap b:");
+scanf("%d",&b);
+printf("nhap c:");
+scanf("%d",&c);
+sum=a+b+c;
+printf("sum=%d",sum);
+
+}
